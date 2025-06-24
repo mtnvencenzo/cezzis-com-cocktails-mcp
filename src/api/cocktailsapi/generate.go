4f373855bc9bf907ca453e8123aed93d8b790832
Ronaldo Vecchi <rvecchi@gmail.com>
2025-06-23 20:09:39 -0600

    feat (mcp): Initial impl api setup - adding cocktails api client gen

---
+++ src/api/cocktailsapi/generate.go
@@ -0,0 +1,3 @@
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config cfg.yaml https://localhost:7176/scalar/v1/openapi.json
+
+package cocktailsapi
---
+++ src/cmd/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"message": "Hello, MCP with standard lib!"})
+	})
+
+	log.Println("Starting MCP server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("could not start server: %s\n", err)
+	}
+}
