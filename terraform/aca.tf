371307149faf57fb09817b533d11872f31fd53bf
mtnvencenzo <rvecchi@gmail.com>
2025-11-26 05:59:45 -0700

    feat (azure): Removing from azure for now

--- terraform/aca.tf
+++ terraform/aca.tf
@@ -1,148 +1,148 @@
-module "aca_cocktails_mcp" {
-  source = "git::ssh://git@github.com/mtnvencenzo/Terraform-Modules.git//modules/container-app"
-  providers = {
-    azurerm = azurerm
-  }
+# module "aca_cocktails_mcp" {
+#   source = "git::ssh://git@github.com/mtnvencenzo/Terraform-Modules.git//modules/container-app"
+#   providers = {
+#     azurerm = azurerm
+#   }
 
-  sub                          = var.sub
-  region                       = var.region
-  environment                  = var.environment
-  domain                       = var.domain
-  name_discriminator           = "mcp"
-  sequence                     = var.sequence
-  tenant_id                    = data.azurerm_client_config.current.tenant_id
-  resource_group_name          = data.azurerm_resource_group.cocktails_resource_group.name
-  resource_group_location      = data.azurerm_resource_group.cocktails_resource_group.location
-  container_app_environment_id = data.azurerm_container_app_environment.cae_shared.id
-  ingress_target_port          = "8080"
-  startup_probe_relative_url   = "/healthz"
+#   sub                          = var.sub
+#   region                       = var.region
+#   environment                  = var.environment
+#   domain                       = var.domain
+#   name_discriminator           = "mcp"
+#   sequence                     = var.sequence
+#   tenant_id                    = data.azurerm_client_config.current.tenant_id
+#   resource_group_name          = data.azurerm_resource_group.cocktails_resource_group.name
+#   resource_group_location      = data.azurerm_resource_group.cocktails_resource_group.location
+#   container_app_environment_id = data.azurerm_container_app_environment.cae_shared.id
+#   ingress_target_port          = "8080"
+#   startup_probe_relative_url   = "/healthz"
 
-  tags = local.tags
+#   tags = local.tags
 
-  container_registry = {
-    id           = data.azurerm_container_registry.shared_acr.id
-    login_server = data.azurerm_container_registry.shared_acr.login_server
-  }
+#   container_registry = {
+#     id           = data.azurerm_container_registry.shared_acr.id
+#     login_server = data.azurerm_container_registry.shared_acr.login_server
+#   }
 
-  key_vault = {
-    id   = data.azurerm_key_vault.cocktails_keyvault.id
-    name = data.azurerm_key_vault.cocktails_keyvault.name
-  }
+#   key_vault = {
+#     id   = data.azurerm_key_vault.cocktails_keyvault.id
+#     name = data.azurerm_key_vault.cocktails_keyvault.name
+#   }
 
-  container = {
-    name       = "cocktails-mcp"
-    image_name = "cocktailsmcp"
-    image_tag  = var.image_tag
-    cpu        = 0.25
-    memory     = "0.5Gi"
-  }
+#   container = {
+#     name       = "cocktails-mcp"
+#     image_name = "cocktailsmcp"
+#     image_tag  = var.image_tag
+#     cpu        = 0.25
+#     memory     = "0.5Gi"
+#   }
 
-  secrets = [
-    {
-      name                  = "apim-host-key"
-      key_vault_secret_name = azurerm_key_vault_secret.cocktails_mcp_apimhostkey.name
-    },
-    {
-      name                  = "apim-cocktails-api-subscription-key"
-      key_vault_secret_name = data.azurerm_key_vault_secret.cocktails_api_mcp_subscription_key.name
-    }
-  ]
+#   secrets = [
+#     {
+#       name                  = "apim-host-key"
+#       key_vault_secret_name = azurerm_key_vault_secret.cocktails_mcp_apimhostkey.name
+#     },
+#     {
+#       name                  = "apim-cocktails-api-subscription-key"
+#       key_vault_secret_name = data.azurerm_key_vault_secret.cocktails_api_mcp_subscription_key.name
+#     }
+#   ]
 
-  env_vars = [
-    {
-      name  = "APPLICATIONINSIGHTS_CONNECTION_STRING"
-      value = data.azurerm_application_insights.appi.connection_string
-    },
-    {
-      name  = "APPLICATIONINSIGHTS_INSTRUMENTATIONKEY"
-      value = data.azurerm_application_insights.appi.instrumentation_key
-    },
-    {
-      name  = "ApplicationInsightsAgent_EXTENSION_VERSION"
-      value = "~2"
-    },
-    {
-      name  = "COCKTAILS_API_HOST"
-      value = "https://api.cezzis.com/prd/cocktails"
-    },
-    {
-      name  = "AUTH0_AUDIENCE"
-      value = var.auth0_audience
-    },
-    {
-      name  = "AUTH0_CLIENT_ID"
-      value = var.auth0_naive_client_id
-    },
-    {
-      name  = "AUTH0_DOMAIN"
-      value = var.auth0_domain
-    },
-    {
-      name  = "AUTH0_SCOPES"
-      value = var.auth0_scopes
-    },
-    {
-      name  = "COSMOS_ACCOUNT_ENDPOINT"
-      value = data.azurerm_cosmosdb_account.cosmosdb_account.endpoint
-    },
-    {
-      name  = "COSMOS_CONNECTION_STRING"
-      value = ""
-    },
-    {
-      name  = "COSMOS_CONTAINER_NAME"
-      value = "cezzis-${var.environment}-device-auth"
-    },
-    {
-      name  = "COSMOS_DATABASE_NAME"
-      value = var.cocktails_cosmosdb_database_name
-    },
-    {
-      name  = "ENV"
-      value = var.environment
-    },
-    {
-      name  = "LOG_LEVEL"
-      value = "info"
-    },
-    {
-      name  = "OTLP_ENDPOINT"
-      value = "https://${data.azurerm_container_app.otel_collector.ingress[0].fqdn}"
-    },
-    {
-      name  = "OTLP_HEADERS"
-      value = "Authorization=Bearer ${data.azurerm_key_vault_secret.otel_collector_api_key.value}"
-    },
-    {
-      name  = "OTLP_INSECURE"
-      value = "false"
-    },
-    {
-      name  = "OTLP_LOG_ENABLED"
-      value = "true"
-    },
-    {
-      name  = "OTLP_METRICS_ENABLED"
-      value = "false"
-    },
-    {
-      name  = "OTLP_TRACE_ENABLED"
-      value = "true"
-    },
-    {
-      name  = "PORT"
-      value = var.port
-    },
-  ]
+#   env_vars = [
+#     {
+#       name  = "APPLICATIONINSIGHTS_CONNECTION_STRING"
+#       value = data.azurerm_application_insights.appi.connection_string
+#     },
+#     {
+#       name  = "APPLICATIONINSIGHTS_INSTRUMENTATIONKEY"
+#       value = data.azurerm_application_insights.appi.instrumentation_key
+#     },
+#     {
+#       name  = "ApplicationInsightsAgent_EXTENSION_VERSION"
+#       value = "~2"
+#     },
+#     {
+#       name  = "COCKTAILS_API_HOST"
+#       value = "https://api.cezzis.com/prd/cocktails"
+#     },
+#     {
+#       name  = "AUTH0_AUDIENCE"
+#       value = var.auth0_audience
+#     },
+#     {
+#       name  = "AUTH0_CLIENT_ID"
+#       value = var.auth0_naive_client_id
+#     },
+#     {
+#       name  = "AUTH0_DOMAIN"
+#       value = var.auth0_domain
+#     },
+#     {
+#       name  = "AUTH0_SCOPES"
+#       value = var.auth0_scopes
+#     },
+#     {
+#       name  = "COSMOS_ACCOUNT_ENDPOINT"
+#       value = data.azurerm_cosmosdb_account.cosmosdb_account.endpoint
+#     },
+#     {
+#       name  = "COSMOS_CONNECTION_STRING"
+#       value = ""
+#     },
+#     {
+#       name  = "COSMOS_CONTAINER_NAME"
+#       value = "cezzis-${var.environment}-device-auth"
+#     },
+#     {
+#       name  = "COSMOS_DATABASE_NAME"
+#       value = var.cocktails_cosmosdb_database_name
+#     },
+#     {
+#       name  = "ENV"
+#       value = var.environment
+#     },
+#     {
+#       name  = "LOG_LEVEL"
+#       value = "info"
+#     },
+#     {
+#       name  = "OTLP_ENDPOINT"
+#       value = "https://${data.azurerm_container_app.otel_collector.ingress[0].fqdn}"
+#     },
+#     {
+#       name  = "OTLP_HEADERS"
+#       value = "Authorization=Bearer ${data.azurerm_key_vault_secret.otel_collector_api_key.value}"
+#     },
+#     {
+#       name  = "OTLP_INSECURE"
+#       value = "false"
+#     },
+#     {
+#       name  = "OTLP_LOG_ENABLED"
+#       value = "true"
+#     },
+#     {
+#       name  = "OTLP_METRICS_ENABLED"
+#       value = "false"
+#     },
+#     {
+#       name  = "OTLP_TRACE_ENABLED"
+#       value = "true"
+#     },
+#     {
+#       name  = "PORT"
+#       value = var.port
+#     },
+#   ]
 
-  env_secret_vars = [
-    {
-      name        = "CocktailsMcp__ApimHostKey"
-      secret_name = "apim-host-key"
-    },
-    {
-      name        = "COCKTAILS_API_XKEY"
-      secret_name = "apim-cocktails-api-subscription-key"
-    }
-  ]
-}
+#   env_secret_vars = [
+#     {
+#       name        = "CocktailsMcp__ApimHostKey"
+#       secret_name = "apim-host-key"
+#     },
+#     {
+#       name        = "COCKTAILS_API_XKEY"
+#       secret_name = "apim-cocktails-api-subscription-key"
+#     }
+#   ]
+# }
