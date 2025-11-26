2bd2f99b711446a2fd5226d62cbb96cc556af1cb
mtnvencenzo <rvecchi@gmail.com>
2025-11-26 06:34:30 -0700

    feat (azure): Removing from azure for now - cleanup

--- terraform/apim-api.tf
+++ terraform/apim-api.tf
@@ -1,86 +1,86 @@
-# module "apim_cocktails_mcp" {
-#   source = "git::ssh://git@github.com/mtnvencenzo/Terraform-Modules.git//modules/apim-api"
-#   providers = {
-#     azurerm = azurerm
-#   }
+module "apim_cocktails_mcp" {
+  source = "git::ssh://git@github.com/mtnvencenzo/Terraform-Modules.git//modules/apim-api"
+  providers = {
+    azurerm = azurerm
+  }
 
-#   tags                           = local.tags
-#   environment                    = var.environment
-#   domain                         = var.domain
-#   name_discriminator             = "mcp"
-#   keyvault_apimhostkey_secret_id = azurerm_key_vault_secret.cocktails_mcp_apimhostkey.id
-#   subscription_required          = false
-#   backend_url_override           = "https://${module.aca_cocktails_mcp.ingress_fqdn}"
-#   service_url_override           = "https://${module.aca_cocktails_mcp.ingress_fqdn}"
+  tags                           = local.tags
+  environment                    = var.environment
+  domain                         = var.domain
+  name_discriminator             = "mcp"
+  keyvault_apimhostkey_secret_id = azurerm_key_vault_secret.cocktails_mcp_apimhostkey.id
+  subscription_required          = false
+  backend_url_override           = "https://${module.aca_cocktails_mcp.ingress_fqdn}"
+  service_url_override           = "https://${module.aca_cocktails_mcp.ingress_fqdn}"
 
-#   apim_instance = {
-#     id                  = data.azurerm_api_management.apim_shared.id
-#     name                = data.azurerm_api_management.apim_shared.name
-#     resource_group_name = data.azurerm_api_management.apim_shared.resource_group_name
-#   }
+  apim_instance = {
+    id                  = data.azurerm_api_management.apim_shared.id
+    name                = data.azurerm_api_management.apim_shared.name
+    resource_group_name = data.azurerm_api_management.apim_shared.resource_group_name
+  }
 
-#   api = {
-#     version      = 1
-#     service_fqdn = module.aca_cocktails_mcp.ingress_fqdn
-#     ingress_fqdn = module.aca_cocktails_mcp.ingress_fqdn
-#   }
+  api = {
+    version      = 1
+    service_fqdn = module.aca_cocktails_mcp.ingress_fqdn
+    ingress_fqdn = module.aca_cocktails_mcp.ingress_fqdn
+  }
 
-#   application_insights = {
-#     id                  = data.azurerm_application_insights.appi.id
-#     instrumentation_key = data.azurerm_application_insights.appi.instrumentation_key
-#     sampling_percentage = 80.0
-#   }
+  application_insights = {
+    id                  = data.azurerm_application_insights.appi.id
+    instrumentation_key = data.azurerm_application_insights.appi.instrumentation_key
+    sampling_percentage = 80.0
+  }
 
-#   subscriptions = [
-#     {
-#       name          = "${title(var.environment)} Cezzis.Com Standard ${title(var.domain)} Mcp Subscription"
-#       primary_key   = random_password.cocktails_mcp_cezzis_com_subscription_keys[0].result
-#       secondary_key = random_password.cocktails_mcp_cezzis_com_subscription_keys[1].result
-#     },
-#     {
-#       name          = "${title(var.environment)} Cezzis.Com Devops ${title(var.domain)} Mcp Subscription"
-#       primary_key   = random_password.cocktails_mcp_devops_subscription_keys[0].result
-#       secondary_key = random_password.cocktails_mcp_devops_subscription_keys[1].result
-#     },
-#     {
-#       name          = "${title(var.environment)} Cezzis.Com LocalUsage ${title(var.domain)} Mcp Subscription"
-#       primary_key   = random_password.cocktails_mcp_localusage_subscription_keys[0].result
-#       secondary_key = random_password.cocktails_mcp_localusage_subscription_keys[1].result
-#     }
-#   ]
+  subscriptions = [
+    {
+      name          = "${title(var.environment)} Cezzis.Com Standard ${title(var.domain)} Mcp Subscription"
+      primary_key   = random_password.cocktails_mcp_cezzis_com_subscription_keys[0].result
+      secondary_key = random_password.cocktails_mcp_cezzis_com_subscription_keys[1].result
+    },
+    {
+      name          = "${title(var.environment)} Cezzis.Com Devops ${title(var.domain)} Mcp Subscription"
+      primary_key   = random_password.cocktails_mcp_devops_subscription_keys[0].result
+      secondary_key = random_password.cocktails_mcp_devops_subscription_keys[1].result
+    },
+    {
+      name          = "${title(var.environment)} Cezzis.Com LocalUsage ${title(var.domain)} Mcp Subscription"
+      primary_key   = random_password.cocktails_mcp_localusage_subscription_keys[0].result
+      secondary_key = random_password.cocktails_mcp_localusage_subscription_keys[1].result
+    }
+  ]
 
-#   operations = [
-#     {
-#       display_name        = "Get Health Status"
-#       method              = "GET"
-#       url_template        = "/healthz"
-#       description         = "Get health"
-#       success_status_code = 200
-#       policy_xml_content  = local.apim_anonomous_operation_policy
-#     },
-#     {
-#       display_name        = "Get Version"
-#       method              = "GET"
-#       url_template        = "/version"
-#       description         = "Get version"
-#       success_status_code = 200
-#       policy_xml_content  = local.apim_anonomous_operation_policy
-#     },
-#     {
-#       display_name        = "POST MCP"
-#       method              = "POST"
-#       url_template        = "/mcp"
-#       description         = "POST MCP"
-#       success_status_code = 200
-#       policy_xml_content  = local.apim_anonomous_operation_policy
-#     },
-#     {
-#       display_name        = "GET MCP"
-#       method              = "GET"
-#       url_template        = "/mcp"
-#       description         = "GET MCP"
-#       success_status_code = 200
-#       policy_xml_content  = local.apim_anonomous_operation_policy
-#     }
-#   ]
-# }
+  operations = [
+    {
+      display_name        = "Get Health Status"
+      method              = "GET"
+      url_template        = "/healthz"
+      description         = "Get health"
+      success_status_code = 200
+      policy_xml_content  = local.apim_anonomous_operation_policy
+    },
+    {
+      display_name        = "Get Version"
+      method              = "GET"
+      url_template        = "/version"
+      description         = "Get version"
+      success_status_code = 200
+      policy_xml_content  = local.apim_anonomous_operation_policy
+    },
+    {
+      display_name        = "POST MCP"
+      method              = "POST"
+      url_template        = "/mcp"
+      description         = "POST MCP"
+      success_status_code = 200
+      policy_xml_content  = local.apim_anonomous_operation_policy
+    },
+    {
+      display_name        = "GET MCP"
+      method              = "GET"
+      url_template        = "/mcp"
+      description         = "GET MCP"
+      success_status_code = 200
+      policy_xml_content  = local.apim_anonomous_operation_policy
+    }
+  ]
+}
--- terraform/cosmosdb.tf
+++ terraform/cosmosdb.tf
@@ -1,39 +1,39 @@
-# module "cocktails_cosmos_containers" {
-#   source = "git::ssh://git@github.com/mtnvencenzo/Terraform-Modules.git//modules/cosmos-db-sql-containers"
+module "cocktails_cosmos_containers" {
+  source = "git::ssh://git@github.com/mtnvencenzo/Terraform-Modules.git//modules/cosmos-db-sql-containers"
 
-#   resource_group_name   = data.azurerm_resource_group.global_shared_resource_group.name
-#   cosmosdb_account_name = data.azurerm_cosmosdb_account.cosmosdb_account.name
-#   database_name         = data.azurerm_cosmosdb_sql_database.cosmosdb_shared_db.name
+  resource_group_name   = data.azurerm_resource_group.global_shared_resource_group.name
+  cosmosdb_account_name = data.azurerm_cosmosdb_account.cosmosdb_account.name
+  database_name         = data.azurerm_cosmosdb_sql_database.cosmosdb_shared_db.name
 
-#   containers = [
-#     {
-#       name                  = "cezzis-${var.environment}-device-auth"
-#       partition_key_paths   = ["/id"]
-#       partition_key_version = 1
-#       indexing_mode         = "consistent"
-#       indexing_path         = "/*"
-#       unique_key_paths      = []
-#     }
-#   ]
-# }
+  containers = [
+    {
+      name                  = "cezzis-${var.environment}-device-auth"
+      partition_key_paths   = ["/id"]
+      partition_key_version = 1
+      indexing_mode         = "consistent"
+      indexing_path         = "/*"
+      unique_key_paths      = []
+    }
+  ]
+}
 
-# module "cocktails_cosmos_role_assignments" {
-#   source = "git::ssh://git@github.com/mtnvencenzo/Terraform-Modules.git//modules/cosmos-db-sql-role-assignments"
+module "cocktails_cosmos_role_assignments" {
+  source = "git::ssh://git@github.com/mtnvencenzo/Terraform-Modules.git//modules/cosmos-db-sql-role-assignments"
 
-#   resource_group_name   = data.azurerm_resource_group.global_shared_resource_group.name
-#   cosmosdb_account_name = data.azurerm_cosmosdb_account.cosmosdb_account.name
-#   database_id           = data.azurerm_cosmosdb_sql_database.cosmosdb_shared_db.id
+  resource_group_name   = data.azurerm_resource_group.global_shared_resource_group.name
+  cosmosdb_account_name = data.azurerm_cosmosdb_account.cosmosdb_account.name
+  database_id           = data.azurerm_cosmosdb_sql_database.cosmosdb_shared_db.id
 
-#   database_role_assignments = [
-#     {
-#       name               = "54d5c255-8508-4ea2-941e-7044fe5db968" # must be a uuid
-#       role_definition_id = data.azurerm_cosmosdb_sql_role_definition.cosmosdb_reader_role.id
-#       principal_id       = module.aca_cocktails_mcp.managed_identity_principal_id
-#     },
-#     {
-#       name               = "57c0645e-bc81-43cd-9607-2377e2660d2a" # must be a uuid
-#       role_definition_id = data.azurerm_cosmosdb_sql_role_definition.cosmosdb_contributor_role.id
-#       principal_id       = module.aca_cocktails_mcp.managed_identity_principal_id
-#     }
-#   ]
-# }
+  database_role_assignments = [
+    {
+      name               = "54d5c255-8508-4ea2-941e-7044fe5db968" # must be a uuid
+      role_definition_id = data.azurerm_cosmosdb_sql_role_definition.cosmosdb_reader_role.id
+      principal_id       = module.aca_cocktails_mcp.managed_identity_principal_id
+    },
+    {
+      name               = "57c0645e-bc81-43cd-9607-2377e2660d2a" # must be a uuid
+      role_definition_id = data.azurerm_cosmosdb_sql_role_definition.cosmosdb_contributor_role.id
+      principal_id       = module.aca_cocktails_mcp.managed_identity_principal_id
+    }
+  ]
+}
